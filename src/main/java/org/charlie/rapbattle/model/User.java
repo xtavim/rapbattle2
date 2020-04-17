4e40929c654496d1df61ee31a8174160a46b2237
Gustavo <gustavo.mendes@live.com.pt>
2020-04-17 09:20:34 +0000

    work

--- src/main/java/org/charlie/rapbattle/model/User.java
+++ src/main/java/org/charlie/rapbattle/model/User.java
@@ -5,7 +5,7 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 @Entity
-@Table(name = "user")
+@Table(name = "user", schema = "rapbattledb")
 public class User {
 
     @Id
